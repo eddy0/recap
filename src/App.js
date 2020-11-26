de2fa6fc31f1ec08c7e452815976a99fbaba4ffa
eddy0 <awindy369@gmail.com>
2020-11-26 13:15:04 -0800

    add init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import  Grid from 'semantic-ui-react'
+
+function App() {
+    return (
+        <Grid container={true}>
+            <Grid.Row centered={true}>
+                React
+            </Grid.Row>
+        </Grid>
+    )
+}
+
+
+export default App
